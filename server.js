e8422ea4f4007bfcb232955c98655a43b2f29e6d
shantanu Amol Patil <netfix@Manalis-MacBook-Pro.local>
2021-03-10 18:10:20 +0530

    commit

--- server.js
+++ server.js
@@ -40,7 +40,12 @@ app.get('/', (req, res) => {
     res.json({ msg: "welcome To My Website" })
 })
 
-
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
